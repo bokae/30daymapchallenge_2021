bfc57feab6711c672853acea8d6b83a931f296dc
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2021-11-05 12:03:59 +0100

    Day 6: cleanup of data

--- day06_red/drawmap.R
+++ day06_red/drawmap.R
@@ -21,10 +21,29 @@ w$pos <- pos[match(w$naam, sel)]
 
 # Read data on dwellings
 dta <- fread("bagger/data/verblijfsobjecten.csv", 
-  select = c(1, 2, 3, 10, 11))
+  select = c(1, 2, 3, 7, 8, 10, 11))
+
+dta[, begin_geldigheid := 
+    as.Date(substr(as.character(begin_geldigheid), 1, 8), "%Y%m%d")]
+dta[, eind_geldigheid := 
+    as.Date(substr(as.character(eind_geldigheid), 1, 8), "%Y%m%d")]
+
+# Remove records that are not valid on reference data
+refdate <- as.Date("2021-09-08")
+dta <- dta[(!is.na(begin_geldigheid) & (begin_geldigheid < refdate)) &
+  (is.na(eind_geldigheid) | (eind_geldigheid > refdate))]
+# We still have duplicates; remove those as well
+dta <- dta[!duplicated(id, fromLast = TRUE), ]
+# Missing values in oppervlakte
+dta <- dta[oppervlakte < 999999]
+# Remove unrealistic dwelling areas
+# dta <- dta[oppervlakte >= 10 & oppervlakte < 2000]
+# Truncate unrealistic dwelling areas
+dta[oppervlakte > 2000, oppervlakte:= 2000]
+dta[oppervlakte < 10, oppervlakte:= 10]
 
 # Data already uses metres; create grid by rounding data
-gridsize <- 3000
+gridsize <- 5000
 dta[, gridx := round(x/gridsize)*gridsize]
 dta[, gridy := round(y/gridsize)*gridsize]
 
@@ -48,7 +67,7 @@ opp <- dta[gebruiksdoel == "woonfunctie", .(
 opp <- opp[gridx > 100 & gridx < 3E5]
 opp <- opp[!(gridx<0.2E5 & gridy>400000)]
 # Filter cells with small numbers
-opp <- opp[n > 80]
+opp <- opp[n > 50]
 
 # Colour scale
 pal <- hcl.colors(20, "Peach")
@@ -66,8 +85,9 @@ lab1[length(lab1)] <- sprintf(">%d", q1[length(q1)-1])
 
 
 # Divide gini into categories
-q2 <- c(0, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.55, 0.60, 0.65, 0.70, 0.75, 
-  0.80, 0.85, 0.90, 0.92, 0.95, 0.98, 0.99, 1)
+q2 <- c(0, 0.1, 0.2, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 027, 0.28, 0.29, 0.30,
+  0.31, 0.32, 0.33, 0.34, 0.35, 0.40, 0.60, 1)
+#q2 <- quantile(opp$gini, probs = seq(0, 1, length.out = length(pal)+1))
 opp[, colgini := as.integer(cut(gini, breaks = q2, right = FALSE, 
   include.lowest = TRUE))]
 lab2 <- sprintf("%4.2f-%4.2f", head(q2,-1), tail(q2, -1))
@@ -75,6 +95,7 @@ lab2 <- sprintf("%4.2f-%4.2f", head(q2,-1), tail(q2, -1))
 
 # =============================================================================
 # Start plot
+png("dwelling_area.png", width = 10, height = 7, res = 300, units = "in")
 par(mar = c(0, 0, 0, 0), oma = c(0 ,0 ,0 ,0)+0.1, mfrow = c(1,2), 
   bg = "#EEEEEE")
 
@@ -86,7 +107,8 @@ rect(opp$gridx, opp$gridy, opp$gridx+gridsize, opp$gridy+gridsize,
 points(w$x, w$y, pch = 20)
 text(w$x, w$y, labels = w$naam, pos = w$pos, cex = 0.8)
 mtext("Median dwelling area (m2)", side = 3, line = -1.2, adj = 0, font = 2)
-legend("bottom", ncol = 5, legend = lab1, fill = pal, bty = 'n', cex = 0.7)
+legend("bottom", ncol = 5, legend = lab1, fill = pal, bty = 'n', cex = 0.7,
+  border = NA)
 
 # Gini
 plot(opp$gridx, opp$gridy, type = 'n', asp = 1, xaxt = "n", yaxt = "n", 
@@ -97,12 +119,16 @@ points(w$x, w$y, pch = 20)
 text(w$x, w$y, labels = w$naam, pos = w$pos, cex = 0.8)
 mtext("Inequality of dwelling area (Gini coefficient dwelling area)", 
   side = 3, line = -1.2, adj = 0, font = 2)
-legend("bottom", ncol = 5, legend = lab2, fill = pal, bty = 'n', cex = 0.7)
+legend("bottom", ncol = 5, legend = lab2, fill = pal, bty = 'n', cex = 0.7, 
+  border = NA)
 
 
 # For testing: plot borders
-mp <- st_read("maps/gm_2021.shp")
-plot(mp[, "geometry"], add = TRUE, border = "gray")
-pv <- st_read("maps/pv_2021.shp")
-plot(pv[, "geometry"], add = TRUE, border = "black")
+# mp <- st_read("maps/gm_2021.shp")
+# plot(mp[, "geometry"], add = TRUE, border = "gray")
+# pv <- st_read("maps/pv_2021.shp")
+# plot(pv[, "geometry"], add = TRUE, border = "black")
+
+dev.off()
+
 
